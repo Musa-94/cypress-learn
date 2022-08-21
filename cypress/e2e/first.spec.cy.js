95383f87c1ceb4e0adc0943146bd63e2e4f9acab
Aynura25 <Aynuraaa25@gmail.com>
2022-08-21 14:27:26 +0400

    add testing in iphone

--- cypress/e2e/first.spec.cy.js
+++ cypress/e2e/first.spec.cy.js
@@ -1,7 +1,14 @@
 describe('mashin.al', () => {
-  it('should open page mahinal shops', () => {
+  it('should open page mahinal shops in macbook', () => {
     cy.viewport('macbook-13')
     cy.visit('https://mashin.al/')
     cy.get('.menu').contains('Salonlar').click('center') 
+  })
+
+  it('should open page mahinal shops in iphone', () => {
+    cy.viewport('iphone-x')
+    cy.visit('https://mashin.al/')
+    cy.get('.icon-burger').click('center') 
+    cy.contains('Salonlar').click('center') 
   })
 })
